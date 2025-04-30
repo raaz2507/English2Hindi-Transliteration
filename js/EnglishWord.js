a615c37ef2274ae0e434c6d9de856fba43f3feee
raaz2507 <raaz2507@gmila.com>
2025-04-30 15:53:28 +0530

    fix styling ishu

--- js/EnglishWord.js
+++ js/EnglishWord.js
@@ -1,3 +1,4 @@
 export const EngWord=['1','2','3','4','5','6','7','8','9','0','too', 'that',"spram","message",
 	"badroom", "bathroom", "bath","postion","style",  "city", "thanks", "smile","restaurant","comment",
-  "ignore","call","impresss","emotion","confuse","market","cameras","bathroom", "spycamera","camera"];
+  "ignore","call","impresss","emotion","confuse","market","cameras","bathroom", "spycamera","camera","mom","but","please","my","this","what","was","can","hmm","just","god", "you", "ur", "how",  "she", "not", "wow", "why","know","now","will", "think","on",
+"of", "with","want", "yes","no","up","get","all","for","have", "after","bad","we", "here","go",];
