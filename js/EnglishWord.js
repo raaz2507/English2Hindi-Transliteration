ce16d64ca1dce750447259c6b13b3b863253081e
raaz2507 <raaz2507@gmila.com>
2025-04-30 21:41:01 +0530

    add words to dic

--- js/EnglishWord.js
+++ js/EnglishWord.js
@@ -1,4 +1,10 @@
 export const EngWord=['1','2','3','4','5','6','7','8','9','0','too', 'that',"spram","message",
 	"badroom", "bathroom", "bath","postion","style",  "city", "thanks", "smile","restaurant","comment",
-  "ignore","call","impresss","emotion","confuse","market","cameras","bathroom", "spycamera","camera","mom","but","please","my","this","what","was","can","hmm","just","god", "you", "ur", "how",  "she", "not", "wow", "why","know","now","will", "think","on",
-"of", "with","want", "yes","no","up","get","all","for","have", "after","bad","we", "here","go",];
+  "ignore","call","impresss","emotion","confuse","market","cameras","bathroom", "spycamera","camera",
+	"mom","but","please","my","this","what","was","can","hmm","just","god", "you", "ur", "how",  "she",
+	 "not", "wow", "why","know","now","will", "think","on","of","happened", "started","any","with","want", 
+	 "yes","no","up","get","all","for","have", "after","bad","done","limit","sakes","while","him","realy",
+	 "if", "would", "mostly","things","over", "simple","from","thing","lets","never", "make","join","we",
+	 "thought","your" ,"went","need","about","said","leave","seriously","at","wanted", "pick","happen", 
+	 "out", "even","age","did",  "thanx","very","still", "gf","again","talk","here","shoping","go","plz",
+   ,	 "mean", "its", "lady", "short", "much", "common"];
